5a78844d6326a71e534b6b157e762361c23bbf5a
Ben Floyd <ben.floyd@simplot.com>
2024-11-30 10:43:34 -0700

    Make link look like wikilink

--- src/commands/FindMissingAttachmentsCommand.ts
+++ src/commands/FindMissingAttachmentsCommand.ts
@@ -21,7 +21,7 @@ export class FindMissingAttachmentsCommand extends BaseCommand {
                     const lineChecks = imageLinks.map(async (imageFile) => {
                         const exists = await this.imageExistsInVault(imageFile);
                         if (!exists) {
-                            const content = `• ${file.path} line ${index + 1}: ${imageFile}`;
+                            const content = `• [[${file.path}]] line ${index + 1}: ${imageFile}`;
                             results.push({
                                 content,
                                 path: file.path,
